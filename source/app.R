7747ec7d9b4ed38c976a997440c7bf21c28e6ef7
Davit Sargsyan <sargdavid@gmail.com>
2018-04-27 22:53:10 -0400

    * Added ICD-9 procedure codes. NOTE: 'major' category is just a copy of 'sub-chapter', too many labels to create by hand. Find a full table online and use it. * Opend new issue (#143)[https://github.com/jackwasey/icd/issues/143] on J. Wasey's 'icd' GitHub page asking for procedure codes

--- source/app.R
+++ source/app.R
@@ -11,17 +11,17 @@
 options(stringsAsFactors = FALSE)
 require(icd)
 require(DT)
-dt0 <- icd9cm_hierarchy
-dt0$short_desc <- NULL
-
-# Display ICD codes along with labels----
-dt1 <- dt0
-dt1$long_desc <- paste(dt1$code,
-                       dt1$long_desc,
-                       sep = ":")
-dt1$major <- paste(dt1$three_digit,
-                       dt1$major,
-                       sep = ":")
+# dt0 <- icd9cm_hierarchy
+# dt0$short_desc <- NULL
+# 
+# # Display ICD codes along with labels----
+# dt1 <- dt0
+# dt1$long_desc <- paste(dt1$code,
+#                        dt1$long_desc,
+#                        sep = ":")
+# dt1$major <- paste(dt1$sub_code,
+#                        dt1$major,
+#                        sep = ":")
 
 # # TEST: bypass user interface!
 # input <- list()
@@ -34,9 +34,12 @@ ui <- fluidPage(
   titlePanel("ICD-9 Clinical Modification Codes & Diagnoses"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "chapter",
-                  label = "Chapter",
-                  choices = unique(as.character(dt1$chapter))),
+      radioButtons(inputId = "dataset",
+                   label = "Select List",
+                   choices = c("Diagnoses",
+                               "Procedures"),
+                   selected = "Diagnoses"),
+      uiOutput(outputId = "chapterIn"),
       uiOutput(outputId = "subchapterIn"),
       uiOutput(outputId = "majorIn"),
       uiOutput(outputId = "dxIn")
@@ -44,18 +47,18 @@ ui <- fluidPage(
     mainPanel(
       DT:: dataTableOutput("tbl"),
       br(),
-      actionButton(inputId = "do", 
+      actionButton(inputId = "do",
                    label = "Save Selection"),
       br(),
       DT:: dataTableOutput("tbl2"),
       br(),
-      downloadLink(outputId = "downloadData", 
+      downloadLink(outputId = "downloadData",
                    label = "Download Selected Rows"),
       br(),
-      downloadLink(outputId = "downloadMap", 
+      downloadLink(outputId = "downloadMap",
                    label = "Download Map of Selected Rows"),
       br(),
-      actionButton(inputId = "reset", 
+      actionButton(inputId = "reset",
                    label = "Reset Table",
                    style = "background-color: #FF0000")
     )
@@ -63,34 +66,73 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  dt0f <- reactive({
+    print(input$dataset)
+    if(input$dataset == "Diagnoses"){
+      dt0 <- icd9cm_hierarchy
+      names(dt0)[names(dt0) == "three_digit"] <- "sub_code"
+    } else {
+      load("icd9cm_sg_hierarchy.RData")
+      dt0 <- icd9cm_sg_hierarchy
+    }
+    dt0$short_desc <- NULL
+    dt0
+  })
+  
+  dt1f <- reactive({
+    # Display ICD codes along with labels----
+    dt1 <- dt0f()
+    dt1$long_desc <- paste(dt1$code,
+                           dt1$long_desc,
+                           sep = ":")
+    dt1$major <- paste(dt1$sub_code,
+                       dt1$major,
+                       sep = ":")
+    dt1
+  })
+  
+  output$chapterIn <- renderUI({
+    dt1 <- dt1f()
+    selectInput(inputId = "chapter", 
+                label = "Chapter", 
+                choices = unique(as.character(dt1$chapter)))
+  })
+  
   output$subchapterIn <- renderUI({
-    selectInput(inputId = "subchapter", 
-                label = "Sub-chapter", 
+    dt1 <- dt1f()
+    selectInput(inputId = "subchapter",
+                label = "Sub-chapter",
                 choices = unique(as.character(dt1$sub_chapter[dt1$chapter == input$chapter])))
   })
-  
+
   output$majorIn <- renderUI({
-    selectInput(inputId = "major", 
-                label = "Major", 
+    dt1 <- dt1f()
+    selectInput(inputId = "major",
+                label = "Major",
                 choices = unique(as.character(dt1$major[dt1$sub_chapter == input$subchapter])))
   })
-  
+
   output$dxIn <- renderUI({
-    selectInput(inputId = "dx", 
-                label = "Diagnosis", 
+    dt1 <- dt1f()
+    selectInput(inputId = "dx",
+                label = "Diagnosis",
                 choices = unique(as.character(dt1$long_desc[dt1$major == input$major])),
                 multiple = TRUE)
   })
-  
+
   # Source: https://yihui.shinyapps.io/DT-rows/
   output$tbl <- DT::renderDT({
+    dt0 <- dt0f()
+    dt1 <- dt1f()
     DT::datatable(unique(dt0[dt1$long_desc %in% input$dx, ]),
                   options = list(pageLength = 10),
                   selection = list(mode = "multiple"))
-  }) 
-  
+  })
+
   # Source: http://shiny.rstudio.com/articles/action-buttons.html
   observeEvent(input$do, {
+    dt0 <- dt0f()
+    dt1 <- dt1f()
     if (!exists("dtt")) {
       dtt <<- unique(dt0[dt1$long_desc %in% input$dx, ])
     } else {
@@ -106,27 +148,27 @@ server <- function(input, output) {
                                      target = "row"))
     })
   })
-  
+
   # Source: https://shiny.rstudio.com/articles/download.html
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("icd9_codes_", 
+      paste("icd9_codes_",
             Sys.Date(),
             ".csv",
             sep = "")
     },
     content = function(file) {
       tmp <- dtt[input$tbl2_rows_selected, ]
-      write.csv(tmp, 
+      write.csv(tmp,
                 file,
                 row.names = FALSE)
     }
   )
-  
+
   # New comorbidity map
   output$downloadMap <- downloadHandler(
     filename = function() {
-      paste("icd9_map_", 
+      paste("icd9_map_",
             Sys.Date(),
             ".RData",
             sep = "")
@@ -143,7 +185,7 @@ server <- function(input, output) {
            file = file)
     }
   )
-  
+
   # Reset diagnoses table
   observeEvent(input$reset, {
     if (exists("dtt")) {
